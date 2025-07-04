e908c737d4f079e2d1eddd38cf45e14f339517d2
Luizwidmar <hdwidmar@gmail.com>
2025-07-04 17:23:15 -0300

    Update DadosPessoaisSection.tsx

--- src/components/perfil/sections/DadosPessoaisSection.tsx
+++ src/components/perfil/sections/DadosPessoaisSection.tsx
@@ -109,15 +109,14 @@ const DadosPessoaisSection: React.FC<DadosPessoaisSectionProps> = ({
               />
             </div>
 
-            <div>
-              <Label htmlFor="data_nascimento">Data de Nascimento</Label>
-              <Input
-                id="data_nascimento"
-                type="date"
-                value={formData.data_nascimento}
-                onChange={(e) => onInputChange('data_nascimento', e.target.value)}
-              />
-            </div>
+          <div>
+            <Label htmlFor="data_nascimento">Data de Nascimento</Label>
+            <DatePicker
+              value={formData.data_nascimento}
+              onChange={(date) => onInputChange('data_nascimento', date)}
+              placeholder="Selecione sua data de nascimento"
+            />
+          </div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
