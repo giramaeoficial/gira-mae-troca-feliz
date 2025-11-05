daca6a4d2ab1dbc288115f53b842d4cfe0f9b232
GiraMãe Oficial <giramaeoficial@gmail.com>
2025-11-05 13:41:17 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://sjyvqtfpvqavtffexoky.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqeXZxdGZwdnFhdnRmZmV4b2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMzczNTksImV4cCI6MjA3NzgxMzM1OX0.BWKEpYOPwpcI_8MFCURt31y4nIY-aqRmmCgBo2H8GOk";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
