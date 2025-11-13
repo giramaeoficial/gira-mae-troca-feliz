61e24aea3d65c60bff870f6dc61803609aad4501
GiraMãe Oficial <giramaeoficial@gmail.com>
2025-11-13 11:59:21 -0300

    Update CotacaoWidget.tsx

--- src/modules/girinhas/components/CotacaoWidget.tsx
+++ src/modules/girinhas/components/CotacaoWidget.tsx
@@ -28,7 +28,7 @@ const CotacaoWidget = () => {
     <Card>
       <CardHeader>
         <CardTitle className="flex items-center gap-2">
-          <img src="/girinha_sem_fundo.png" alt="girinha" className="h-5 w-5"/>
+          <img src="/girinha_sem_fundo.png" alt="girinha" className="h-10 w-auto"/>
           Preço das Girinhas
         </CardTitle>
         <CardDescription>
