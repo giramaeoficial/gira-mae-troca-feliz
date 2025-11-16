fdf8b7a8ab0b3446b4ab8d5532e23646703be33c
GiraMãe Oficial <giramaeoficial@gmail.com>
2025-11-16 10:57:40 -0300

    Update Header.tsx

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -163,7 +163,7 @@ const Header = () => {
             {/* -------------- LOGO -------------- */}
             <Link to="/feed" className="flex items-center gap-2">
               <img src="/giramae_logo.png" alt="Logo GiraMãe"
-                className="h-16 w-auto object-contain" />
+                className="h-12 w-auto object-contain" />
               
             </Link>
 
