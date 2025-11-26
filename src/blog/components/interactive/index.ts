f7e339991f2f48fab6752930d7a77ff9ee7247d0
GiraMãe Oficial <giramaeoficial@gmail.com>
2025-11-26 16:58:40 -0300

    Update index.ts

--- src/blog/components/interactive/index.ts
+++ src/blog/components/interactive/index.ts
@@ -1,1 +1,9 @@
-// Exportações dos componentes interativos
+// src/blog/components/interactive/index.ts
+
+// Exporta todos os componentes interativos do blog
+export { default as CalculadoraGastosRoupas } from './CalculadoraGastosRoupas';
+export { default as SimuladorTamanhoRoupa } from './SimuladorTamanhoRoupa';
+
+// Adicione novos exports conforme criar componentes:
+// export { default as QuizMaternidade } from './QuizMaternidade';
+// export { default as ComparadorPrecos } from './ComparadorPrecos';
